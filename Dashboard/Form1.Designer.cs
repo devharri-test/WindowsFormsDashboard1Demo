8aa7c0a82543d68962d2047fa83ae72ab62f83de
Devharri <devharri@outlook.com>
2021-12-15 14:32:12 +0200

    Added ButtonRound class to project for toolbox use

--- Dashboard/Form1.Designer.cs
+++ Dashboard/Form1.Designer.cs
@@ -173,31 +173,31 @@ namespace Dashboard
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(36, 117);
+            this.label2.Location = new System.Drawing.Point(37, 119);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.Size = new System.Drawing.Size(125, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Some user text here";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(138)))), ((int)(((byte)(193)))));
-            this.label1.Location = new System.Drawing.Point(54, 95);
+            this.label1.Location = new System.Drawing.Point(56, 100);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.Size = new System.Drawing.Size(87, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "User Name";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Dashboard.Properties.Resources.Yksivärinen_neliö_Valkoinen;
-            this.pictureBox1.Location = new System.Drawing.Point(60, 25);
+            this.pictureBox1.Location = new System.Drawing.Point(59, 19);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(63, 63);
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
---
+++ Dashboard/UserControls/ButtonRound.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dashboard.UserControls
+{
+    class ButtonRound : Control
+    {
+        private readonly Label lbl;
+        public ButtonRound() : base()
+        {
+            lbl = new Label
+            {
+                Text = Text,
+                ForeColor = ForeColor,
+                BackColor = BackColor,
+                Font = Font
+            };
+            CenterInParent();
+        }
+        private void CenterInParent()
+        {
+            lbl.Left = (Width - lbl.Width) / 2;
+            lbl.Top = (Height - lbl.Height) / 2;
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            GraphicsPath grPath = new GraphicsPath();
+            grPath.AddEllipse(0, 0, (ClientSize.Width-2), (ClientSize.Height-2));
+            Region = new Region(grPath);
+            base.OnPaint(e);
+        }
+        protected override void OnMove(EventArgs e)
+        {
+            CenterInParent();
+            base.OnMove(e);
+        }
+        protected override void OnTextChanged(EventArgs e)
+        {
+            lbl.Text = Text;
+            base.OnTextChanged(e);
+        }
+        protected override void OnForeColorChanged(EventArgs e)
+        {
+            lbl.ForeColor = ForeColor;
+            base.OnForeColorChanged(e);
+        }
+        protected override void OnBackColorChanged(EventArgs e)
+        {
+            lbl.BackColor = BackColor;
+            base.OnBackColorChanged(e);
+        }
+        protected override void OnFontChanged(EventArgs e)
+        {
+            lbl.Font = Font;
+            base.OnFontChanged(e);
+        }
+    }
+}
